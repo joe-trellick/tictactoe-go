14da93b60cb49c7c47622a91957e9c27bda16ce4
Joe Hughes <joe.hughes.code@gmail.com>
2021-05-11 08:09:56 +0100

    Add structs for players

--- tictactoe.go
+++ tictactoe.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type player struct {
+	name  string
+	piece rune
+}
+
 func main() {
+	player1 := player{name: "Player 1", piece: '❌'}
+	player2 := player{name: "Player 2", piece: '⭕'}
+	players := [2]player{player1, player2}
+	turn := 0
+
 	fmt.Print("\n\nWelcome to TIC ❌ TAC ⭕ TOE\n\n\n")
+
+	currentPlayer := players[turn%1]
+	fmt.Println("Current player: " + currentPlayer.name + " " + string(currentPlayer.piece))
 }
