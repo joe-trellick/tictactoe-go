4ffad7d9f18c0a1716c8e725111b697cc8372152
Joe Hughes <joe.hughes.code@gmail.com>
2021-05-11 08:59:03 +0100

    Added board printing, test current player code

--- tictactoe.go
+++ tictactoe.go
@@ -7,14 +7,47 @@ type player struct {
 	piece rune
 }
 
+func printBoard(board [3][3]rune) {
+	for rowIndex, row := range board {
+		if rowIndex > 0 {
+			fmt.Println("─┼─┼─")
+		}
+		for colIndex, val := range row {
+			if colIndex > 0 {
+				fmt.Print("│")
+			}
+			if val == 0 {
+				squareIndex := rowIndex*3 + colIndex + 1
+				fmt.Print(squareIndex)
+			} else {
+				var boardVal string
+				if val == '❌' {
+					boardVal = "X"
+				} else {
+					boardVal = "O"
+				}
+				fmt.Print(boardVal)
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
 func main() {
+	var board [3][3]rune
+
 	player1 := player{name: "Player 1", piece: '❌'}
 	player2 := player{name: "Player 2", piece: '⭕'}
 	players := [2]player{player1, player2}
-	turn := 0
+	turn := 1
+
+	board[2][1] = '❌'
 
 	fmt.Print("\n\nWelcome to TIC ❌ TAC ⭕ TOE\n\n\n")
 
-	currentPlayer := players[turn%1]
+	printBoard(board)
+
+	currentPlayer := players[turn%2]
 	fmt.Println("Current player: " + currentPlayer.name + " " + string(currentPlayer.piece))
 }
