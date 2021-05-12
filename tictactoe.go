de2c167db26a571b26a4a911fd66449cf75e4994
Joe Hughes <joe.hughes.code@gmail.com>
2021-05-12 08:00:13 +0100

    Add player input (no game validation yet)

--- tictactoe.go
+++ tictactoe.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
 type player struct {
 	name  string
@@ -8,6 +12,7 @@ type player struct {
 }
 
 func printBoard(board [3][3]rune) {
+	fmt.Println()
 	for rowIndex, row := range board {
 		if rowIndex > 0 {
 			fmt.Println("━╋━╋━")
@@ -17,7 +22,7 @@ func printBoard(board [3][3]rune) {
 				fmt.Print("┃")
 			}
 			if val == 0 {
-				squareIndex := rowIndex*3 + colIndex + 1
+				squareIndex, _ := indicesToSquareNumber(rowIndex, colIndex)
 				fmt.Print(squareIndex)
 			} else {
 				var boardVal string
@@ -34,20 +39,58 @@ func printBoard(board [3][3]rune) {
 	fmt.Println()
 }
 
+func squareNumberToIndices(number int) (rowIndex int, colIndex int, err error) {
+	if number < 1 || number > 9 {
+		return 0, 0, errors.New("Number is out of range 1 - 9")
+	}
+
+	rowIndex = (number - 1) / 3
+	colIndex = (number - 1) % 3
+	return
+}
+
+func indicesToSquareNumber(rowIndex int, colIndex int) (squareNumber int, err error) {
+	if rowIndex < 0 || rowIndex >= 3 {
+		return 0, errors.New("rowIndex out of range")
+	}
+	if colIndex < 0 || colIndex >= 3 {
+		return 0, errors.New("colIndex out of range")
+	}
+
+	return rowIndex*3 + colIndex + 1, nil
+}
+
 func main() {
 	var board [3][3]rune
 
 	player1 := player{name: "Player 1", piece: '❌'}
 	player2 := player{name: "Player 2", piece: '⭕'}
 	players := [2]player{player1, player2}
-	turn := 1
+	turn := 0
+	var hasWinner bool
 
-	board[2][1] = '❌'
+	fmt.Print("\n\nWelcome to TIC ❌ TAC ⭕ TOE\n\n")
 
-	fmt.Print("\n\nWelcome to TIC ❌ TAC ⭕ TOE\n\n\n")
+	for !hasWinner {
 
-	printBoard(board)
+		printBoard(board)
 
-	currentPlayer := players[turn%2]
-	fmt.Println("Current player: " + currentPlayer.name + " " + string(currentPlayer.piece))
+		currentPlayer := players[turn%2]
+		fmt.Println(string(currentPlayer.piece) + " " + currentPlayer.name + ", please enter your move:")
+		var moveInput string
+		fmt.Scanln(&moveInput)
+
+		number, inputError := strconv.Atoi(moveInput)
+		if inputError != nil {
+			fmt.Println("Please enter a square number from the board")
+		} else {
+			rowIndex, colIndex, inputError := squareNumberToIndices(number)
+			if inputError != nil {
+				fmt.Println("Please enter a square number from the board")
+			} else {
+				board[rowIndex][colIndex] = currentPlayer.piece
+				turn += 1
+			}
+		}
+	}
 }
